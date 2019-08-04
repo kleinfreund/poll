5867234b99470ffada5a1a7944f728d54b426293
kleinfreund <philipprudloff@fastmail.com>
2019-08-04 19:36:45 +0200

    Update compiled modules

--- dist/cjs/poll.js
+++ dist/cjs/poll.js
@@ -4,16 +4,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * Calls a given function and keeps calling it after the specified delay has passed.
  *
  * @param {() => any} fn The function to call.
- * @param {Number} delay The delay (in milliseconds) to wait before calling the function again.
- * @param {() => Boolean} shouldStopPolling A callback function indicating whether to stop polling.
+ * @param {number} delay The delay (in milliseconds) to wait before calling the function again.
+ * @param {() => boolean} shouldStopPolling A callback function indicating whether to stop polling.
  */
 async function poll(fn, delay, shouldStopPolling = () => false) {
     if (typeof delay !== 'number') {
         throw new TypeError(`Expected “delay” to be of type number, but it was of type ${typeof delay}.`);
     }
-    else if (delay < 0) {
-        throw new RangeError(`Expected “delay” to be a non-negative number, but it was “${delay}”.`);
-    }
+    delay = Math.max(0, delay);
     try {
         do {
             await fn();

8160097323ef1f31c7e9caaa9d25a9f9194be1df
kleinfreund <philipprudloff@fastmail.com>
2022-04-27 13:20:05 +0200

    Tweak type definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,19 @@
 /**
  * Calls a given function and keeps calling it after the specified delay has passed.
- *
- * @param fn The function to call.
- * @param delayOrDelayCallback The delay (in milliseconds) to wait before calling the function again. Can be a function.
- * @param shouldStopPolling A callback function indicating whether to stop polling.
  */
 export declare function poll(
+  /**
+   * The function to call.
+   */
   fn: () => any,
+
+  /**
+   * The delay (in milliseconds) to wait before calling the function again. Can be a function.
+   */
   delayOrDelayCallback: number | (() => number),
+
+  /**
+   * A callback function indicating whether to stop polling.
+   */
   shouldStopPolling: () => boolean | Promise<boolean>
 ): Promise<void>

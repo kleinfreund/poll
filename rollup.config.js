5e442f9cec2903c66f99d03a4b03f52a331091a7
kleinfreund <philipprudloff@fastmail.com>
2022-11-16 22:24:17 +0100

    chore(deps): updates dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'rollup'
+import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
-import { terser } from 'rollup-plugin-terser'
 
 export default defineConfig({
   input: 'src/poll.ts',
